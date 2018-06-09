f6d323c5fab8cf11dce87f71aa224d346990ac72
MadNoodle <mjanneau@gmail.com>
2018-06-09 11:35:38 +0200

    Initial Commit : Boilerplate

---
+++ RxDeezer/RxDeezer/PlaylistDisplayer/PlaylistDisplayerViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PlaylistDisplayerViewController.swift
+//  RxDeezer
+//
+//  Created by Mathieu Janneau on 09/06/2018.
+//  Copyright © 2018 Mathieu Janneau. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class PlaylistDisplayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+
+}
---
+++ RxDeezer/RxDeezer/PlaylistDisplayer/PlaylistDisplayerViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PlaylistDisplayerViewModel.swift
+//  RxDeezer
+//
+//  Created by Mathieu Janneau on 09/06/2018.
+//  Copyright © 2018 Mathieu Janneau. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class PlaylistDisplayerViewModel {
+  
+}
